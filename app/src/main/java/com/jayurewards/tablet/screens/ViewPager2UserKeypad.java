7da7858af9dc945caf0389a59038e5dbc90d6ea4
Natalie Parker <nataliep0817@gmail.com>
2021-05-13 22:43:29 -0700

    Added ViewPager2 dependency and started on the viewpager in UserKeypadScreen

---
+++ app/src/main/java/com/jayurewards/tablet/screens/ViewPager2UserKeypad.java
@@ -0,0 +1,24 @@
+package com.jayurewards.tablet.screens;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewPager2UserKeypad extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
